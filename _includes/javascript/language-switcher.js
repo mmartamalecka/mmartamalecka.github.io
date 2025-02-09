d27fd25357485a537c71cdaed97504e9dc2b0a30
kuba <jk.ziolkowski@gmail.com>
2025-02-09 21:16:31 +0100

    ładny switcher

--- _includes/javascript/language-switcher.js
+++ _includes/javascript/language-switcher.js
@@ -8,6 +8,16 @@ function switchLanguage(lang) {
     document.querySelectorAll(`.lang-${lang}`).forEach(el => {
       el.style.display = 'block';
     });
+
+    const activeButton = document.querySelector(`.button-${lang}`);
+    const inactiveButton = document.querySelector(`.button-${lang === 'en' ? 'pl' : 'en'}`);
+
+    if (activeButton && inactiveButton) {
+      activeButton.style.display = 'none';
+      activeButton.style.padding = '0';
+      inactiveButton.style.display = '';
+      inactiveButton.style.padding = ''; 
+    }
   }
   
   function applyLanguage() {
